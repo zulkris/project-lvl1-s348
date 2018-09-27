7af57192e36a01927c8fe85b36980b1dec570d9f
zulkris <2garin.zmej@gmail.com>
2018-09-28 01:16:13 +0300

    fix linter

--- src/games/calc.php
+++ src/games/calc.php
@@ -3,7 +3,6 @@ namespace BrainGames\Games\Calc;
 
 const GAME_NAME = 'Calc';
 const DESCRIPTION = 'What is the result of the expression?';
-const STEPS = 3;
 
 function getQuestion()
 {
@@ -12,7 +11,7 @@ function getQuestion()
     $randnum1 = rand(0, 100);
     $randnum2 = rand(0, 100);
     return "$randnum1 ".$signs[0]." $randnum2";
-};
+}
 
 function getRightAnswer($expression)
 {
@@ -34,8 +33,7 @@ function getRightAnswer($expression)
     }
 
     return $result;
-};
-
+}
 function letsplay()
 {
     $gameData = function () {
