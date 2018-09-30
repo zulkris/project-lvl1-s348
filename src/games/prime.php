4cd7e60410e029c0b8a45eda148ad9efb7a97d52
zulkris <2garin.zmej@gmail.com>
2018-09-30 18:56:59 +0300

    fix

--- src/games/prime.php
+++ src/games/prime.php
@@ -12,7 +12,7 @@ function isPrime($number)
     if ($number <= 1) {
         return false;
     }
-    for ($i = 2; $i <= $number/2; $i++) {
+    for ($i = 2; $i <= $number / 2; $i++) {
         if (($number % $i) == 0) {
             return false;
         }
